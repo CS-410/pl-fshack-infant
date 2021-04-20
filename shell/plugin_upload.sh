f83755bd73442877b3d7b7b0ad0aa3b7a58680ba
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2021-04-20 19:53:53 -0400

    add CUBE plugin uploader

---
+++ shell/plugin_upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Add plugins specifically for the COVIDNET feedflow.
+#
+
+./plugin_add.sh  "fnndsc/pl-fshack-infant"
+
+#
+# Adding additional users
+# Users can be added using some specific variation of the
+# following:
+#
+# CUBE USERS
+# For "superusers"
+#####user_add.sh -U "rudolph:rudolph1234:rudolph.pienaar@gmail.com"
+# For "normal users"
+#####user_add.sh    "rpienaar:rudolph1234:rppienaar@gmail.com"
+#
+# STORE USERS
+# For "superusers"
+#####user_add.sh -U -S "rudolph:rudolph1234:rudolph.pienaar@gmail.com"
+# For "normal users"
+#####user_add.sh -S    "rpienaar:rudolph1234:rppienaar@gmail.com"
+#
